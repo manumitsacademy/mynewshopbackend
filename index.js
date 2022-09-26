29c251c4be0cd1a945e14e80046de56835ffa869
edupoly <info.edupoly@gmail.com>
2022-09-26 16:31:20 +0530

    authentication

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 // server.js
+var express = require('express');
+var app = express()
 const jsonServer = require('json-server')
 var path = require("path")
 const lodash = require("lodash");
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+var cors = require('cors')
+var bodyParser=require('body-parser');
+server.use(cors())
 server.use(jsonServer.bodyParser)
+const jwt = require('jsonwebtoken');
+var users = [
+    {
+        username:'praveen',
+        password:'123',
+        userId:12
+    },
+    {
+        username:'prateek',
+        password:'222',
+        userId:47
+    },
+    {
+        username:'alok',
+        password:'333',
+        userId:17
+    },
+]
+server.post("/login",function(req,res){
+    console.log(req.body)
+    var s = users.find(function(a){
+        if(a.username===req.body.username && a.password===req.body.password){
+            return true
+        }
+    })
+    if(s){
+        const token = jwt.sign(req.body, "some secret");
+        console.log(token)
+        res.json({token:token});
+    }
+    else{
+        console.log("erro")
+        res.json({err:'passwordnotmatched'})
+    }
+})
+
+// server.get("/foodItems",function(req,res){
+//     let allproducts = router.db.get("foodItems").valueOf();
+//     res.json(allproducts)
+// })
+
 let productTypes = [];
 let allproducts = router.db.get("products").valueOf();
 productTypes = allproducts.map((product)=>{
@@ -17,6 +63,13 @@ productTypes.forEach((t)=>{
     var productsByType = lodash.groupBy(allproducts,{type:t}).true;
     productsByTypes[t]=productsByType;
 })    
+server.use((req,res,next)=>{
+    console.log("server middleware ")
+    next();
+})
+server.get("/hi",(req,res)=>{
+    res.send("Please wiat")
+})
 server.use((req,res,next)=>{
     console.log(req.url);
     console.log(req.url.split("/"));
